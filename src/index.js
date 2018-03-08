66463c44d5b93c2647d5d85bc23f13a0626252c2
Kranuh <timkranen@hotmail.com>
2018-03-09 00:15:37 +0100

    Added Rellax

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // Can also be included with a regular script tag
 import Typed from 'typed.js';
+import Rellax from 'rellax';
 
 var options = {
   strings: ["<i>> am an Android developer.</i>", "<i>> am a Software Engineer.</i>", "<i>> design User Experiences.</i>", "<i>> love movies.</i>", "<i>> care about code standards.</i>", "<i>> always use Git.</i>"],
   typeSpeed: 30,
-  fadeOut: true,
+  fadeOut: false,
   fadeOutClass: 'typed-fade-out',
   fadeOutDelay: 500,
   showCursor: true,
@@ -15,3 +16,4 @@ var options = {
 }
 
 var typed = new Typed(".element", options);
+var rellax = new Rellax('.rellax');
